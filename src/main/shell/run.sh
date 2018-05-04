53dc96312ff2df8a4672cc8bb0196149f8be14c7
Mahendran Ponnusamy <mponnusamy@cas.org>
2018-05-04 10:46:35 -0400

    Refactored the code. Added SparkSQL Feature.

--- src/main/shell/run.sh
+++ src/main/shell/run.sh
@@ -2,7 +2,7 @@ sudo rm -rf ~/work/shared/*
 sudo cp /home/mqp29/workspace/streaming-app/target/streaming-app-1.0.1-SNAPSHOT.jar ~/work/shared/ 
 sudo chmod 777 -R /home/mqp29/work/shared/
 
-CMD="spark2-submit --name Mahendran --class com.icc.poc.StreamingApplication --conf spark.yarn.submit.waitAppCompletion=false --master yarn --deploy-mode cluster --queue edhops /work/shared/streaming-app-1.0.1-SNAPSHOT.jar"
+CMD="spark2-submit --name Mahendran --class com.icc.poc.StreamingSQLApplication --conf spark.yarn.submit.waitAppCompletion=false --master yarn --deploy-mode cluster --queue edhops /work/shared/streaming-app-1.0.1-SNAPSHOT.jar"
 
 echo $CMD
 docker exec -u hdfs node1 $CMD
